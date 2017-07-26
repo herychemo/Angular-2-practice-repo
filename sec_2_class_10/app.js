eca79a64fdaf087e97fa88c5be032eb3b89f510e
Heriberto Reyes Esparza <hery.chemo@gmail.com>
2017-07-26 15:11:54 -0500

    clase 10

---
+++ sec_2_class_10/app.js
@@ -0,0 +1,8 @@
+"use strict";
+var WHAT = "Constantes en Mayus por convencion ";
+var mensaje = "Hola Externo";
+if (true) {
+    var mensaje_1 = "adios Hermano";
+}
+console.log(mensaje);
+console.log(WHAT);
---
+++ sec_2_class_10/app.ts
@@ -0,0 +1,10 @@
+
+const WHAT = "Constantes en Mayus por convencion "
+let mensaje = "Hola Externo"
+
+if(true){
+  let mensaje = "adios Hermano"
+}
+
+console.log(mensaje);
+console.log(WHAT);
