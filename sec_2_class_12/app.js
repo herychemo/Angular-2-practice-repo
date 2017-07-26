901f4b1abc01218ef0f675f29e850ac601938cf2
Heriberto Reyes Esparza <hery.chemo@gmail.com>
2017-07-26 17:00:48 -0500

    clase 12

---
+++ sec_2_class_12/app.js
@@ -0,0 +1,18 @@
+"use strict";
+var nombre = "Petter";
+var numero = 123;
+var booleano = true;
+var hoy = new Date();
+var cualquiera;
+cualquiera = "";
+cualquiera = 123;
+cualquiera = hoy;
+cualquiera = false;
+var spiderman = {
+    nombre: "Petter",
+    edad: 20
+};
+spiderman = {
+    nombre: "Juan",
+    edad: 2,
+};
---
+++ sec_2_class_12/app.ts
@@ -0,0 +1,23 @@
+
+let nombre:string = "Petter";
+let numero :number= 123;
+let booleano:boolean = true;
+
+let hoy:Date = new Date();
+
+
+let cualquiera:any ;
+cualquiera = "";
+cualquiera = 123;
+cualquiera = hoy;
+cualquiera = false;
+
+let spiderman = {
+    nombre : "Petter",
+    edad : 20
+};
+
+spiderman = {
+    nombre : "Juan",
+    edad:2,
+};
