cfe80145d83674fe63b9b7f85ec5a70337bc9f06
Heriberto Reyes Esparza <hery.chemo@gmail.com>
2017-07-31 13:05:35 -0500

    implemented base for heroes service

--- Projects/2.-SPA/src/app/components/heroes/heroes.component.ts
+++ Projects/2.-SPA/src/app/components/heroes/heroes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Heroe, HeroesService} from '../../services/heroes.service';
 
 @Component({
   selector: 'app-heroes',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroesComponent implements OnInit {
 
-  constructor() { }
+    private heroes:any[] = []
 
-  ngOnInit() {
-  }
+    constructor(  private _HeroesService:HeroesService  ) {
+        console.log("HeroesComponent constructor()");
+    }
+
+    ngOnInit() {
+        console.log("HeroesComponent ngOnInit()");
+        this.heroes = this._HeroesService.getHeroes();
+    }
 
 }
