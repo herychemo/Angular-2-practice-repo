933027bac93c13c5b9f9ea4159aad124013b3aee
Heriberto Reyes Esparza <hery.chemo@gmail.com>
2017-08-05 16:28:46 -0500

    Go to Spanish Language

--- Projects/3.-Pipes/src/app/app.module.ts
+++ Projects/3.-Pipes/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { LOCALE_ID } from "@angular/core";
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+      { provide:LOCALE_ID, useValue:"es" }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
