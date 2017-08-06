686d9b6104d7abf3d85a9bf8c37aa5c8867553ad
Heriberto Reyes Esparza <hery.chemo@gmail.com>
2017-08-05 19:13:43 -0500

    implemented base for rouytes

--- Projects/4.-SpotiApp/src/app/app.module.ts
+++ Projects/4.-SpotiApp/src/app/app.module.ts
@@ -1,18 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
+/** Rutas */
+import {APP_ROUTING} from "./app.routes";
+
+/** Servicios */
+
+
+
+/** Components */
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
+import { SearchComponent } from './components/search/search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    HomeComponent
+    HomeComponent,
+    SearchComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    APP_ROUTING
   ],
   providers: [],
   bootstrap: [AppComponent]
