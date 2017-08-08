8c6a0229914741f2076faefbcca5f1aab4fd6eae
Heriberto Reyes Esparza <hery.chemo@gmail.com>
2017-08-07 19:59:20 -0500

    starting with service

---
+++ Projects/4.-SpotiApp/src/app/services/spotify.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+
+import { Http } from '@angular/http';
+import 'rxjx/add/operator/map'
+
+@Injectable()
+export class SpotifyService {
+
+    artists:any[] = [];
+
+    urlSearch : string = "https://api.spotify.com/v1/search";
+
+  constructor(private http:Http) { }
+
+  getArtists(term:string){
+      let query = `?q=${term}&type=artist`;
+      let url = this.urlSearch + query;
+
+      return this.http.get(url)
+                .map( res => {
+                    console.log(res);
+                });
+  }
+
+}
